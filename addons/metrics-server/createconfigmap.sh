f2ce305ce3b544de922a5d715b19259ab1930131
Patrick Blaas <patrick@kite4fun.nl>
2018-08-01 11:24:12 +0200

    small wrapper script to use proper certs

--- addons/metrics-server/createconfigmap.sh
+++ addons/metrics-server/createconfigmap.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+if [ $1 ]; then
 kubectl -n kube-system create configmap metricsconfig \
-  --from-file=sa-k8s.pem=../../tls/sa-calico-k8s.pem \
-  --from-file=sa-k8s-key.pem=../../tls/sa-calico-k8s-key.pem \
+  --from-file=sa-k8s.pem=../../tls/sa-$1-k8s.pem \
+  --from-file=sa-k8s-key.pem=../../tls/sa-$1-k8s-key.pem \
   --from-file=kubeconfig=kubeconfig
+else
+echo Syntax $0 createconfigmap.sh clustername
+fi
