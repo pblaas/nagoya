2c3665f2d32c9cb79e4675b3eead07848c036735
pblaas <patrick@kite4fun.nl>
2018-10-26 11:45:20 +0200

    Bump version

--- addnode.py
+++ addnode.py
@@ -9,7 +9,7 @@ from jinja2 import Environment, FileSystemLoader
 
 __author__ = "Patrick Blaas <patrick@kite4fun.nl>"
 __license__ = "GPL v3"
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 __status__ = "Active"
 
 PATH = os.path.dirname(os.path.abspath(__file__))
