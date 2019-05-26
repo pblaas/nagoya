5122eb820ad37015131fdec66cd84495252b6c2d
pblaas <patrick@kite4fun.nl>
2019-05-26 20:32:12 +0200

    Bump metrics server to 0.3.3

--- addons/metrics-server/createconfigmap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $1 ]; then
-kubectl -n kube-system create configmap metricsconfig \
-  --from-file=sa-k8s.pem=../../tls/sa-$1-k8s.pem \
-  --from-file=sa-k8s-key.pem=../../tls/sa-$1-k8s-key.pem \
-  --from-file=kubeconfig=kubeconfig
-else
-echo Syntax $0 createconfigmap.sh clustername
-fi
