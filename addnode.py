bcbd700f68afcb99829ff14e55339291e9e7cccf
pblaas <patrick@kite4fun.nl>
2019-07-05 10:48:21 +0200

    adding additional variable to addnode

--- addnode.py
+++ addnode.py
@@ -79,6 +79,7 @@ try:
         openssltemplate = (etcd_openssl_template.render(
             ipaddress=nodeip,
             loadbalancer=(subnetcidr).rsplit('.', 1)[0] + ".3"
+            firstmaster=(subnetcidr).rsplit('.', 1)[0] + ".10"
         ))
 
         with open('./tls/openssl.cnf', 'w') as openssl:
